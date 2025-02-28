b45c8e1ece82b8b23aa90767b7b3c6d91871c539
TONY GAO <TONY.GAO@STU.OCSB.CA>
2025-02-28 17:38:54 +0000

    Added

--- area_per_rect_input.py
+++ area_per_rect_input.py
@@ -8,17 +8,18 @@
 
 
 def main():
+    # Gets length and width from user input
     length = int(input("Enter Length: "))
     width = int(input("Enter Width: "))
-
+    # Calculates the perimeter and area
     area = length * width
     perimeter = 2 * (length + width)
-
+    #prints the length, width, area and perimeter
     print("Length is:", length)
     print("Width is:", width)
     print("Area is:", area)
     print("Perimeter is:", perimeter)
 
+
 if __name__ == "__main__":
     main()
-
